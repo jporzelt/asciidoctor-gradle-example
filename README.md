62ecc861838d1bd27a2c18776f59a739602b003c
johannes.porzelt@canoo.com <HorseBatBoogie09>
2017-01-14 13:11:29 +0100

    added example

---
+++ README.md
@@ -0,0 +1,6 @@
+Run
+`gradlew build asciidoctor`
+
+And the see the generated doc at
+`build/docs/html5/example.html`
+`build/docs/pdf/example.pdf`
---
+++ example-java/src/main/java/PublicAPI.java
@@ -0,0 +1,11 @@
+public interface PublicAPI {
+
+    // tag::apiMethod[]
+    /**
+     *
+     * @param parameter A string that will be returned again
+     * @return The given parameter string
+     */
+    String helloWorld (String parameter);
+    // end::apiMethod[]
+}
